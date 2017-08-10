a3e4b45ecd863c3f7dfdcfb1f260005ccb34bc38
Cory Lucas <cory@corylucas.com>
2017-08-09 22:38:12 -0400

    use calloc to make sure pointers are set to null

--- src/lunch_menu_item_layer.c
+++ src/lunch_menu_item_layer.c
@@ -20,26 +20,27 @@ static void update_date_text(LunchMenuItemLayer *lunch_menu_item_layer) {
 }
 
 LunchMenuItemLayer *lunch_menu_item_layer_create(GRect frame) {
-    LunchMenuItemLayer *self = malloc(sizeof(LunchMenuItemLayer));
+    LunchMenuItemLayer *self = calloc(1, sizeof(LunchMenuItemLayer));
 
-    self->date_text = malloc(sizeof(char) * DATE_TEXT_LENGTH);
+    self->date_text = calloc(DATE_TEXT_LENGTH, sizeof(char));
 
     self->wrapper_layer = layer_create(frame);
 
     GRect date_text_layer_bounds = frame;
-    date_text_layer_bounds.size.h = 30;
+    date_text_layer_bounds.size.h = 22;
     self->date_text_layer = text_layer_create(date_text_layer_bounds);
-    text_layer_set_font(self->date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    text_layer_set_font(self->date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
     text_layer_set_text_alignment(self->date_text_layer, GTextAlignmentCenter);
     text_layer_set_text(self->date_text_layer, self->date_text);
     layer_add_child(self->wrapper_layer, text_layer_get_layer(self->date_text_layer));
 
     GRect menu_text_layer_bounds = frame;
-    menu_text_layer_bounds.origin.y = 30;
-    menu_text_layer_bounds.size.h -= 30;
+    menu_text_layer_bounds.origin.x = 4;
+    menu_text_layer_bounds.origin.y = 22;
+    menu_text_layer_bounds.size.w -= 8;
+    menu_text_layer_bounds.size.h -= 24;
     self->menu_text_layer = text_layer_create(menu_text_layer_bounds);
     text_layer_set_font(self->menu_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-    text_layer_set_text_alignment(self->menu_text_layer, GTextAlignmentCenter);
     layer_add_child(self->wrapper_layer, text_layer_get_layer(self->menu_text_layer));
 
     return self;
@@ -73,7 +74,7 @@ void lunch_menu_item_layer_set_date(LunchMenuItemLayer *lunch_menu_item_layer, t
 }
 
 const char *lunch_menu_item_layer_get_menu(LunchMenuItemLayer *lunch_menu_item_layer) {
-    return text_layer_get_text(lunch_menu_item_layer->menu_text_layer);
+    return lunch_menu_item_layer->menu_text;
 }
 
 void lunch_menu_item_layer_set_menu(LunchMenuItemLayer *lunch_menu_item_layer, char *menu_text, uint16_t menu_length) {
